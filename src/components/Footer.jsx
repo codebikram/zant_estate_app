bde3f6774f93b27b3fa9d7c3b7bc53d0e4a74118
Mondal <2143543@cognizant.com>
2024-01-26 20:28:55 +0530

    added chatbot icon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,18 @@
 import { Link } from 'react-router-dom';
+import { TbMessageChatbot } from 'react-icons/tb';
 
 const Footer = () => {
   return (
-    <footer className='flex bg-slate-100 flex-wrap justify-between  items-center text-slate-700 text-sm'>
-      <p className='p-2 sm:p-5'>
-        Need Help?
-        <Link to='/about' className='text-blue-700 font-bold hover:underline'>
-          Chat with Us
+    <footer className=' bg-slate-100 text-slate-700'>
+      <p className='p-2 sm:p-5 fixed bottom-0 right-0 z-50 text-7xl'>
+        <Link to='/about' className='text-blue-500 font-bold hover:underline '>
+          <TbMessageChatbot />
         </Link>
       </p>
-      <p className='p-2 sm:p-5'>Copyright &copy; 2024 All rights reserved</p>
+
+      <p className='p-2 sm:p-5 text-center text-sm'>
+        Copyright &copy; 2024 All rights reserved
+      </p>
     </footer>
   );
 };
